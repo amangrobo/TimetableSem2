ec816f6ecb96e3db38f28a1ded1339bc916bf8fa
amangrobo <amangrobo@gmail.com>
2018-12-21 21:38:56 +0530

    bug fix

--- app/src/main/java/com/grobo/timetablesem2/MainActivity.java
+++ app/src/main/java/com/grobo/timetablesem2/MainActivity.java
@@ -78,8 +78,8 @@ public class MainActivity extends AppCompatActivity{
 
     @Override
     public void onBackPressed() {
-        moveTaskToBack(true);
-        android.os.Process.killProcess(android.os.Process.myPid());
-        System.exit(1);
+        finish();
+        System.exit(0);
+        super.onBackPressed();
     }
 }
