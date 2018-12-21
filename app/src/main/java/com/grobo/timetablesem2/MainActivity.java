5b72c30e23419907a031dfcde5aca67e92d31530
chanmol1999 <chanmol1999@gmail.com>
2018-12-21 16:57:35 +0530

    Added function of refresh

--- app/src/main/java/com/grobo/timetablesem2/MainActivity.java
+++ app/src/main/java/com/grobo/timetablesem2/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -73,6 +74,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        switch(id)
+        {
+            case R.id.update:
+                startActivity(new Intent(MainActivity.this, FirstActivity.class));
+                break;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
         return true;
     }
 }
