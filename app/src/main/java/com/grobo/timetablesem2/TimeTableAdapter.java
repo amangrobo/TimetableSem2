4777ee67a3d34a119a5d1b1f3339315bce82b4f9
amangrobo <amangrobo@gmail.com>
2018-12-30 20:22:34 +0530

    change layout weight according to 1701 and 1801

--- app/src/main/java/com/grobo/timetablesem2/TimeTableAdapter.java
+++ app/src/main/java/com/grobo/timetablesem2/TimeTableAdapter.java
@@ -4,14 +4,20 @@ import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class TimeTableAdapter extends ArrayAdapter<SingleDay> {
+
+    String branchPre;
 
     public TimeTableAdapter(Activity context, int resource, List<SingleDay> objects) {
         super(context, resource, objects);
+        branchPre = context.getSharedPreferences("PREFERENCE", MODE_PRIVATE).getString("branchPre", "");
     }
 
     @Override
@@ -22,6 +28,13 @@ public class TimeTableAdapter extends ArrayAdapter<SingleDay> {
 
         TextView timeTextView = (TextView) convertView.findViewById(R.id.time_view);
         TextView subjectTextView = (TextView) convertView.findViewById(R.id.subject_view);
+
+        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) subjectTextView.getLayoutParams();
+
+        if (branchPre.compareTo("1701") == 0){
+            params.weight = 2.0f;
+            subjectTextView.setLayoutParams(params);
+        }
 
         SingleDay singleTimetable = getItem(position);
 
