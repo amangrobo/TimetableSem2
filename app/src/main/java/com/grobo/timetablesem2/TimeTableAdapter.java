f1f4fb61ba17bd28d293f1cd95770b1f85e5d024
amangrobo <amangrobo@gmail.com>
2018-12-30 20:43:57 +0530

    updates

--- app/src/main/java/com/grobo/timetablesem2/TimeTableAdapter.java
+++ app/src/main/java/com/grobo/timetablesem2/TimeTableAdapter.java
@@ -1,6 +1,7 @@
 package com.grobo.timetablesem2;
 
 import android.app.Activity;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
